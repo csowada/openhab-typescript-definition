47feca94a8807a645775e1beb43eba9a384d8e80
Christian S <git@cs-dev.de>
2018-12-29 18:08:10 +0100

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,75 @@
+/// <reference path="./typings/custom.d.ts" />
+/// <reference path="./typings/java-oh.d.ts" />
+
 /// <reference path="./typings/java.d.ts" />
 /// <reference path="./typings/nashorn.d.ts" />
 /// <reference path="./typings/openhab2.d.ts" />
-/// <reference path="./typings/esh/automation/Action.d.ts" />
+
 /// <reference path="./typings/ruleset-default.d.ts" />
+/// <reference path="./typings/ruleset-rulefactories.d.ts" />
+/// <reference path="./typings/ruleset-rulesupport.d.ts" />
+/// <reference path="./typings/ruleset-simplerule.d.ts" />
+/// <reference path="./typings/ruleset-timer.d.ts" />
 
 /// <reference path="./typings/esh/automation/Action.d.ts" />
 /// <reference path="./typings/esh/automation/Condition.d.ts" />
 /// <reference path="./typings/esh/automation/Module.d.ts" />
 /// <reference path="./typings/esh/automation/Rule.d.ts" />
+/// <reference path="./typings/esh/automation/Trigger.d.ts" />
+/// <reference path="./typings/esh/automation/Visibility.d.ts" />
 
+/// <reference path="./typings/esh/automation/type/ActionType.d.ts" />
+/// <reference path="./typings/esh/automation/type/ConditionType.d.ts" />
+/// <reference path="./typings/esh/automation/type/Input.d.ts" />
+/// <reference path="./typings/esh/automation/type/ModuleType.d.ts" />
+/// <reference path="./typings/esh/automation/type/Output.d.ts" />
+/// <reference path="./typings/esh/automation/type/TriggerType.d.ts" />
 
+/// <reference path="./typings/esh/config/core/ConfigDescriptionParameter.d.ts" />
+/// <reference path="./typings/esh/config/core/Configuration.d.ts" />
+
+/// <reference path="./typings/esh/core/common/registry/Identifiable.d.ts" />
+
+/// <reference path="./typings/esh/core/events/Event.d.ts" />
+
+/// <reference path="./typings/esh/core/items/ActiveItem.d.ts" />
+/// <reference path="./typings/esh/core/items/GenericItem.d.ts" />
+/// <reference path="./typings/esh/core/items/GroupItem.d.ts" />
+/// <reference path="./typings/esh/core/items/Item.d.ts" />
 /// <reference path="./typings/esh/core/items/ItemRegistry.d.ts" />
+
+/// <reference path="./typings/esh/core/library/types/DateTimeType.d.ts" />
+/// <reference path="./typings/esh/core/library/types/DecimalType.d.ts" />
+/// <reference path="./typings/esh/core/library/types/HSBType.d.ts" />
+/// <reference path="./typings/esh/core/library/types/IncreaseDecreaseType.d.ts" />
+/// <reference path="./typings/esh/core/library/types/OnOffType.d.ts" />
+/// <reference path="./typings/esh/core/library/types/OpenClosedType.d.ts" />
+/// <reference path="./typings/esh/core/library/types/PercentType.d.ts" />
+
+/// <reference path="./typings/esh/core/persistence/HistoricItem.d.ts" />
+
+/// <reference path="./typings/esh/core/types/Command.d.ts" />
+/// <reference path="./typings/esh/core/types/ComplexType.d.ts" />
+/// <reference path="./typings/esh/core/types/PrimitiveType.d.ts" />
+/// <reference path="./typings/esh/core/types/State.d.ts" />
+/// <reference path="./typings/esh/core/types/Type.d.ts" />
+/// <reference path="./typings/esh/core/types/UnDefType.d.ts" />
+
+/// <reference path="./typings/esh/core/util/ActionBuilder.d.ts" />
+/// <reference path="./typings/esh/core/util/ConditionBuilder.d.ts" />
+/// <reference path="./typings/esh/core/util/ModuleBuilder.d.ts" />
+/// <reference path="./typings/esh/core/util/RuleBuilder.d.ts" />
+/// <reference path="./typings/esh/core/util/TriggerBuilder.d.ts" />
+
+/// <reference path="./typings/esh/module/script/ScriptExtensionProvider.d.ts" />
+/// <reference path="./typings/esh/module/script/defaultscope/internal/ScriptBusEvent.d.ts" />
+
+/// <reference path="./typings/esh/rulesupport/AutomationManager.d.ts" />
+
+/// <reference path="./typings/esh/rulesupport/shared/ScriptedHandler.d.ts" />
+/// <reference path="./typings/esh/rulesupport/shared/simple/SimpleActionHandler.d.ts" />
+/// <reference path="./typings/esh/rulesupport/shared/simple/SimpleConditionHandler.d.ts" />
 /// <reference path="./typings/esh/rulesupport/shared/simple/SimpleRule.d.ts" />
+/// <reference path="./typings/esh/rulesupport/shared/simple/SimpleRuleActionHandler.d.ts" />
+/// <reference path="./typings/esh/rulesupport/shared/simple/SimpleTriggerHandler.d.ts" />
+/// <reference path="./typings/esh/rulesupport/shared/simple/SimpleTriggerHandlerCallback.d.ts" />
